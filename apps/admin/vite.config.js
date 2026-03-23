c33ac242633e3e347ed7584df0ac5e286de16c21
Michael Odada <mikeodada333@gmail.com>
2026-03-23 19:59:45 +0300

    fix import path

--- apps/admin/vite.config.js
+++ apps/admin/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -10,7 +11,9 @@ export default defineConfig({
         target: 'http://localhost:5001',
         changeOrigin: true,
         secure: false,
+
       },
     },
   },
 })
+
